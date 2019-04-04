58ad6ec6cea291c2432535cfbaf9bd9ce9822684
Marina Manso Jimeno <marinaa246@gmail.com>
2019-04-04 19:26:13 +0200

    Temporal fix for JS GUID bug (register tab)

--- src/coms/coms_ui/static/phantomAutofill.js
+++ src/coms/coms_ui/static/phantomAutofill.js
@@ -4,7 +4,7 @@ This script autopopulates the input fields when phantom is selected as a subject
 ----------
 
 Author: Marina Manso;
-Date: 03/18/2019
+Date: 03/25/2019
 Version 0.0
 Copyright of the Board of Trustees of  Columbia University in the City of New York
 """
@@ -18,23 +18,43 @@ $(document).ready(() => {
   cannot be changed by the user*/
   if ($('#phantomopt').is(":selected") == true) {
 
-    //Changing input fields
-    // TODO: Change this to input numbers from 0000 to 9999
-    $("#patid").val(1+Math.floor(Math.random() * 9999));
+
+    $("#patid").val(1+Math.floor(Math.random() * 9999)); //Random numbers
+    //Random alpha-numeric user ID
+    // FIXME: Not working
+    /*var randomId = uuidv1(); // -> v1 UUID
+    $("#patid").val(randomId);*/
     $("#patid").prop("readonly", true);
     $("#name").val("ISMRM-NIST");
     $("#name").prop("readonly", true);
-    $("#age").val("20");
+    $("#age").val("00");
     $("#age").prop("readonly", true);
-    $("#dob").val("12/12/2012");
+
+    //Today's date
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    /*if(dd < 10){
+      dd='0'+dd;
+    }
+    if(mm < 10){
+       mm="0"+mm;
+    }*/
+    today = mm+'/'+dd+'/'+yyyy;
+
+    $("#dob").val(today);
     $("#dob").prop("readonly", true);
     $("#met").prop("checked", true);
     $("#imp").prop("disabled", true);
     $("#opt3").prop("checked", true);
     $("#opt2").prop("disabled", true);
     $("#opt1").prop("disabled", true);
+    $("#weight").val("3");
+    $("#weight").prop("readonly", true);
     $("#height").val("20");
     $("#height").prop("readonly", true);
+    $("#extraImpFields").hide();
     $('#orient option:not(:selected)').attr('disabled', true);
     $('#anatomy option:not(:selected)').attr('disabled', true);
 
