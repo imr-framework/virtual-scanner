c71403844d2967379e5ac56babd6d99a66c38437
Enlin Qian <eq2144@columbia.edu>
2019-04-09 16:50:31 -0400

    Fix bugs and improve the speed

--- src/server/quant_analysis/ROI_analysis.py
+++ src/server/quant_analysis/ROI_analysis.py
@@ -2,7 +2,7 @@
 This script does ROI analysis of dicom maps
 
 Author: Enlin Qian
-Date: 04/01/2019
+Date: 04/08/2019
 Version 1.0
 Copyright of the Board of Trustees of  Columbia University in the City of New York
 """
@@ -15,6 +15,26 @@ import os
 import cv2
 from scipy.spatial import distance
 import numpy.matlib
+
+
+def circle_analysis(circles, map_size):
+    """
+    Return locations of pixels inside a circle
+
+    Parameters
+    ----------
+    circles: 1x3 matrix (center_x, center_y, radius)
+    map_size: size of map, for example, 256 means the size of map is 256x256
+
+    Returns
+    -------
+    sphere_map: binary map where pixels inside the circle is true and outside of the circle is false
+    """
+    X, Y = np.meshgrid(np.arange(map_size), np.arange(map_size))
+    distance_map = np.sqrt(np.square(X - circles[0]) + np.square(Y - circles[1]))
+    sphere_map = distance_map <= circles[2]
+    return sphere_map
+
 
 def main(dicom_map_path: str, intensity_range_lb: np, intensity_range_ub: np, map_size: np):  # lb: lower bound, ub: upper bound
     """
@@ -53,37 +73,10 @@ def main(dicom_map_path: str, intensity_range_lb: np, intensity_range_ub: np, ma
         map_data_final[:, :, n1] = np.divide(map_data_final[:, :, n1], np.amax(map_data_final[:, :, n1]))
         map_data_final[:, :, n1] = np.multiply(map_data_final[:, :, n1], max_values[n1])  # conversion to original intensity (this is desired)
 
-    map_data_final_gray = np.matrix.copy(map_data_final[:, :, 0])  # set up a new variable used for image processing and circle detection
     num_spheres = 14
-    pixel_indices = np.logical_or((map_data_final_gray <= intensity_range_lb), (map_data_final_gray >= intensity_range_ub))
-    map_data_final_gray[pixel_indices] = 0  # threshold so that pixels outside frequency ranges are 0
-
-    map_data_final_gray = np.divide(map_data_final_gray, np.amax(map_data_final_gray))
-    map_data_final_gray = np.multiply(map_data_final_gray, 255)  # conversion to grayscale for open cv operations
-
-    map_data_final_gray = map_data_final_gray.astype(np.uint8)  # open cv requires uint8 grayscale images
-    circles = cv2.HoughCircles(map_data_final_gray, cv2.HOUGH_GRADIENT, dp=1, minDist=13,
-                               param1=150, param2=8, minRadius=5, maxRadius=7)  # initial guess of spheres
-
-    centers = circles[0, :, :2]
-    radii = 0.8*np.matlib.repmat(np.amin(circles[0, :, 2]), num_spheres, 1)
-
-    location_map = np.zeros((image_size[0], image_size[1], num_spheres))
-    for n2 in range(num_spheres):
-        for n3 in range(image_size[0]):
-            for n4 in range(image_size[1]):
-                location_map[n3, n4, n2] = np.sqrt(np.square(centers[n2, 1]-n3-1)+np.square(centers[n2, 0]-n4-1))  # create a meshgrid (space for optimization)
-
-    mean_all = np.zeros([num_spheres, 1])
-    for n5 in range(num_spheres):
-        temp1 = location_map[:, :, n5]
-        mean_all[n5] = np.mean(map_data_final[temp1 <= radii[n5, 0]])  # retrieve points inside the circles
-
-    indices = np.argmax(mean_all)
-    sphere_1_loc = np.concatenate((centers[indices], radii[indices]))  # find the loc and radius of sphere 1
-
     scale = map_size / 256
-    direction_angle_mat = np.array([[0, 0],
+    rot_center = np.array([[128/2, map_size/2]])  # This need to be changed after new experiments are done
+    direction_angle_mat = np.array([[0, 0],  # first one is Sphere 1
                                    [scale*19.6764, 90-18.1284],
                                    [scale*35.9758, 90-35.9850],
                                    [scale*48.8824, 90-54.2402],
@@ -96,33 +89,51 @@ def main(dicom_map_path: str, intensity_range_lb: np, intensity_range_ub: np, ma
                                    [scale*44.5752, 90-74.4948],
                                    [scale*22.2815, -(90-55.8365)],
                                    [scale*21.8366, 90-56.2077],
-                                   [scale*43.8830, -(90-73.3844)]]) # matrix with distance and angle for each sphere respect to sphere 1
+                                   [scale*43.8830, -(90-73.3844)]])  # matrix with distance and angle for each sphere respect to sphere 1
+    sphere_all_loc_template = np.zeros([num_spheres, 2])
+    sphere_all_loc_template[0, 0:2] = np.squeeze(np.array([[62.9708, 98.2607]]))
 
-    sphere_all_loc = np.zeros([num_spheres, 3])
-    sphere_all_loc[0, :] = sphere_1_loc
-    for n6 in range(1, num_spheres):
-        sphere_all_loc[n6, 0] = direction_angle_mat[n6, 0]*np.sin(np.pi*direction_angle_mat[n6, 1]/180)+sphere_all_loc[0,0]
-        sphere_all_loc[n6, 1] = direction_angle_mat[n6, 0]*np.cos(np.pi*direction_angle_mat[n6, 1]/180)+sphere_all_loc[0,1]
-        sphere_all_loc[n6, 2] = sphere_all_loc[0, 2]
+    for n2 in range(1,
+                    num_spheres):  # get all sphere centers in the template (use distance instead of just coordinates because of scaling)
+        sphere_all_loc_template[n2, 0] = direction_angle_mat[n2, 0] * np.sin(np.pi * direction_angle_mat[n2, 1] / 180) + \
+                                         sphere_all_loc_template[0, 0]
+        sphere_all_loc_template[n2, 1] = direction_angle_mat[n2, 0] * np.cos(np.pi * direction_angle_mat[n2, 1] / 180) + \
+                                         sphere_all_loc_template[0, 1]
+    angle_template_vec = np.array([[sphere_all_loc_template[0, 0]-rot_center[0, 0], sphere_all_loc_template[0, 1]-rot_center[0, 1]]])
+
+    sphere_1_loc_map = np.zeros([1, 3, image_size[2]])
+    sphere_all_loc_map = np.zeros([num_spheres, 3, image_size[2]])
+    for n3 in range(image_size[2]):  # process all maps in the folder
+        map_data_final_gray = np.matrix.copy(map_data_final[:, :, n3])  # set up a new variable used for image processing and circle detection
+        pixel_indices = np.logical_or((map_data_final_gray <= intensity_range_lb), (map_data_final_gray >= intensity_range_ub))
+        map_data_final_gray[pixel_indices] = 0  # threshold so that pixels outside frequency ranges are 0
+        map_data_final_gray = np.divide(map_data_final_gray, np.amax(map_data_final_gray))
+        map_data_final_gray = np.multiply(map_data_final_gray, 255)  # conversion to grayscale for open cv operations
+        map_data_final_gray = map_data_final_gray.astype(np.uint8)  # open cv requires uint8 grayscale images
+        circles = cv2.HoughCircles(map_data_final_gray, cv2.HOUGH_GRADIENT, dp=1, minDist=13,
+                               param1=150, param2=8, minRadius=5, maxRadius=7)  # initial guess of spheres
+        circles_all = circles[0, :, :]
+        circles_all[:, 2] = np.squeeze(0.8*np.matlib.repmat(np.amin(circles[0, :, 2]), circles.shape[1], 1))  # use smallest radius for calculating means
+        for n4 in range(circles.shape[1]):  # loop through all the circles (may not be same number as num_spheres due to false positives)
+            circle_loc = circle_analysis(circles_all[n4, :], map_size)  # get binary map
+            circle_mean = np.mean(map_data_final[np.where(circle_loc)[0]])
+            sphere_1_loc_map[0, :, n3] = circles_all[np.argmax(circle_mean)]  # find the loc and radius of sphere 1 for all maps
+
+        angle_map_vec = np.array([[sphere_1_loc_map[0, 0, n3]-rot_center[0, 0], sphere_1_loc_map[0, 1, n3]-rot_center[0, 1]]])
+        rot_angle = np.arccos(np.divide(np.dot(np.squeeze(angle_template_vec), np.squeeze(angle_map_vec)), np.linalg.norm(angle_template_vec)*np.linalg.norm(angle_map_vec)))  # calcualte rot angle
+        rot_angle = rot_angle*np.pi/180
+        rot_mat = np.array([[np.cos(rot_angle), -np.sin(rot_angle)], [np.sin(rot_angle), np.cos(rot_angle)]])
+        sphere_all_loc_map[:, 0:2, n3] = np.squeeze(np.matmul(sphere_all_loc_template, rot_mat))
+        sphere_all_loc_map[:, 2, n3] = circles_all[:, 2]
 
     plt.figure()
     plt.imshow(map_data_final[:, :, 0], cmap='hot')
     ax = plt.gca()
 
-    for i in sphere_all_loc[:]:
+    for n5 in range(num_spheres):
         # draw the center of the circle
-        circle_plot = plt.Circle((i[0], i[1]), i[2], color='r', fill=False)
+        circle_plot = plt.Circle((sphere_all_loc_map[n5, 0, 0], sphere_all_loc_map[n5, 1, 0]), sphere_all_loc_map[n5, 2, 0], color='r', fill=False)
         ax.add_artist(circle_plot)
     plt.show()
-
-    
-
-
-
-
-
-
-
-
 
 
