fccc1ca321a5e113b060136a00bfd8d9b419c9df
Keerthi Sravan Ravi <sravan953@gmail.com>
2019-06-21 12:51:01 -0400

    Migrate reconstruct.py from os.path to pathlib.Path

--- virtualscanner/utils/constants.py
+++ virtualscanner/utils/constants.py
@@ -3,6 +3,7 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent.parent
 
 COMS_PATH = ROOT_PATH / 'coms'
+RECON_PATH = ROOT_PATH / 'recon'
 SERVER_ANALYZE_PATH = COMS_PATH / 'server' / 'ana'
 SERVER_PATH = ROOT_PATH / 'server'
 STATIC_ANALYZE_PATH = Path('static') / 'ana'
