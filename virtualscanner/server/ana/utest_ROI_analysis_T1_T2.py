628f0f09f3f533fb734245c140599aa78831fe8f
Enlin Qian <eq2144@columbia.edu>
2019-07-02 13:08:38 -0400

    Modify unit testing files to improve build speed

--- virtualscanner/server/ana/utest_ROI_analysis_T1_T2.py
+++ virtualscanner/server/ana/utest_ROI_analysis_T1_T2.py
@@ -11,9 +11,10 @@ from virtualscanner.utils import constants
 
 SERVER_T2_MAP_INPUT_PATH = constants.SERVER_ANALYZE_PATH / 'inputs' / 'T2_map'
 
-dicomROIanalysis.main(
-    dicom_map_path=SERVER_T2_MAP_INPUT_PATH,
-    map_type='T1',
-    map_size='128',
-    fov='210',
-    pat_id='9306')
+if __name__ == '__main__':
+    dicomROIanalysis.main(
+        dicom_map_path=SERVER_T2_MAP_INPUT_PATH,
+        map_type='T1',
+        map_size='128',
+        fov='210',
+        pat_id='9306')
