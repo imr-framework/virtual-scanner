e6259dbc2d147265be1ff1b9c5a504a3b7ec70c8
Sairam Geethanath <sairamgeethanath@gmail.com>
2019-07-15 14:53:12 -0400

    Overwrite existing files in asset folder for Tx/SAR

--- virtualscanner/coms/coms_ui/coms_server_flask.py
+++ virtualscanner/coms/coms_ui/coms_server_flask.py
@@ -455,6 +455,9 @@ def worker():
 
                 filename = filename[:-4] + '.seq'
 
+                if(os.path.isfile(constants.SERVER_PATH / 'rf' / 'tx' / 'SAR_calc' / 'assets' / filename)):
+                    os.remove(constants.SERVER_PATH / 'rf' / 'tx' / 'SAR_calc' / 'assets' / filename)
+
                 os.rename(upload_path, constants.SERVER_PATH / 'rf' / 'tx' / 'SAR_calc' / 'assets' / filename)
 
                 output = SAR_calc_main.payload_process(filename)
