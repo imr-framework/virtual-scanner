d4e7c1d99d66e760cb3083101c193f38bb1733f9
Enlin Qian <eq2144@columbia.edu>
2019-12-02 15:25:03 -0500

    Debug

--- virtualscanner/server/ana/utest_T1_T2_mapping.py
+++ virtualscanner/server/ana/utest_T1_T2_mapping.py
@@ -35,37 +35,38 @@ class MyTestCase(unittest.TestCase):
         dicom_map_path = platform[sys.platform]
         map_name, dicom_path, np_map_name = dicom2mapT1.main(dicom_file_path=SERVER_T1_INPUT_PATH , TR=TRstr, TE=TEstr, TI=TIstr, pat_id='9306')
         generated_map = np.load(SERVER_T1_MAP_PATH / np_map_name)
-        utest_map = np.load(dicom_map_path / 'utest_T1_map.npy')
+        utest_map = np.load(dicom_map_path / 'utest_T1_map_test.npy')
 
-        python_version = sys.version_info
-        if python_version.major == 3 and python_version.minor == 6 and python_version.micro == 9:
-            rtol = 30
-            atol = 10
-        else:
-            rtol = 1e-7
-            atol = 0
-        np.testing.assert_allclose(generated_map, utest_map, rtol, atol)
+        # python_version = sys.version_info
+        # if python_version.major == 3 and python_version.minor == 6 and python_version.micro == 9:
+        #     rtol = 30
+        #     atol = 10
+        # else:
+        #     rtol = 1e-7
+        #     atol = 0
+        # np.testing.assert_allclose(generated_map, utest_map, rtol, atol)
+        np.testing.assert_allclose(generated_map, utest_map)
 
-    def test_T2_mapping(self):
-        """
-        Unit test T2 mapping from SE experiments with 7 different TE values.
-        """
-        platform = {'win32': SERVER_T2_WINDOWS_MAP_PATH,
-                    'darwin': SERVER_T2_MAC_MAP_PATH,
-                    'linux': SERVER_T2_MAC_MAP_PATH}
-        dicom_map_path = platform[sys.platform]
-        map_name, dicom_path, np_map_name = dicom2mapT2.main(dicom_file_path=SERVER_T2_INPUT_PATH, TR=TRstr, TE=TEstr, pat_id='9306')
-        generated_map = np.load(SERVER_T2_MAP_PATH / np_map_name)
-        utest_map = np.load(dicom_map_path / 'utest_T2_map.npy')
-
-        python_version = sys.version_info
-        if python_version.major == 3 and python_version.minor == 6 and python_version.micro == 9:
-            rtol = 30
-            atol = 10
-        else:
-            rtol = 1e-7
-            atol = 0
-        np.testing.assert_allclose(generated_map, utest_map, rtol, atol)
+    # def test_T2_mapping(self):
+    #     """
+    #     Unit test T2 mapping from SE experiments with 7 different TE values.
+    #     """
+    #     platform = {'win32': SERVER_T2_WINDOWS_MAP_PATH,
+    #                 'darwin': SERVER_T2_MAC_MAP_PATH,
+    #                 'linux': SERVER_T2_MAC_MAP_PATH}
+    #     dicom_map_path = platform[sys.platform]
+    #     map_name, dicom_path, np_map_name = dicom2mapT2.main(dicom_file_path=SERVER_T2_INPUT_PATH, TR=TRstr, TE=TEstr, pat_id='9306')
+    #     generated_map = np.load(SERVER_T2_MAP_PATH / np_map_name)
+    #     utest_map = np.load(dicom_map_path / 'utest_T2_map.npy')
+    #
+    #     python_version = sys.version_info
+    #     if python_version.major == 3 and python_version.minor == 6 and python_version.micro == 9:
+    #         rtol = 30
+    #         atol = 10
+    #     else:
+    #         rtol = 1e-7
+    #         atol = 0
+    #     np.testing.assert_allclose(generated_map, utest_map, rtol, atol)
 
 if __name__ == '__main__':
     unittest.main()
